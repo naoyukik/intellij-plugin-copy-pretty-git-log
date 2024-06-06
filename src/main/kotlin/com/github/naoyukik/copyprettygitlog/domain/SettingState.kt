666dc53f37d7ddead85801ab2a350f7dd802325e
Kodama Naoyuki <mmaga@dstribe.net>
2024-06-06 22:48:31 +0900

    refactor: Remove unused getAppSettingsState method in SettingState

--- src/main/kotlin/com/github/naoyukik/copyprettygitlog/domain/SettingState.kt
+++ src/main/kotlin/com/github/naoyukik/copyprettygitlog/domain/SettingState.kt
@@ -2,13 +2,8 @@ package com.github.naoyukik.copyprettygitlog.domain
 
 import com.github.naoyukik.copyprettygitlog.settings.AppSettingsState
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.editor.Editor
 
 class SettingState {
-    fun getAppSettingsState(editor: Editor): AppSettingsState? {
-        return editor.project?.let { AppSettingsState.getInstance(it) }
-    }
-
     fun getAppSettingsState(e: AnActionEvent): AppSettingsState? {
         return e.project?.getService(AppSettingsState::class.java)
     }
